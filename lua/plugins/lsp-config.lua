97ee841c3469774bbfc52fe7f60ba8bed960a1ed
Jannik Donker <jado96@web.de>
2024-04-07 13:23:49 +0200

    update - more lsp stuff

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,13 +16,26 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+			local capabilities = require('cmp_nvim_lsp').default_capabilities()
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.tsserver.setup({})
-			lspconfig.svelte.setup({})
-			lspconfig.tailwindcss.setup({})
-			lspconfig.cssls.setup({})
-			lspconfig.rust_analyzer.setup({})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities
+			})
+			lspconfig.svelte.setup({
+				capabilities = capabilities
+			})
+			lspconfig.tailwindcss.setup({
+				capabilities = capabilities
+			})
+			lspconfig.cssls.setup({
+				capabilities = capabilities
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities
+			})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
