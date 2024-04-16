fc40bc496d334ef58f33faaaa3ddd0e185665c1f
Jannik Donker <jado96@web.de>
2024-04-16 12:59:51 +0200

    deleted copilot chat, fixed plenary

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.6",
 		dependencies = {
-			"vim-lua/plenary.nvim",
+			"nvim-lua/plenary.nvim",
 		},
 		config = function()
 			local builtin = require("telescope.builtin")
