82949130a92398b0e9129aa37358047f1412421e
Jannik Donker <jado96@web.de>
2024-10-29 08:46:17 +0100

    updated

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,32 +9,41 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver", "svelte", "tailwindcss", "cssls", "rust_analyzer" },
+				ensure_installed = { "lua_ls", "ts_ls", "svelte", "tailwindcss", "cssls", "rust_analyzer" },
 			})
 		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({
-				capabilities = capabilities
+				capabilities = capabilities,
 			})
-			lspconfig.tsserver.setup({
-				capabilities = capabilities
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
 			})
 			lspconfig.svelte.setup({
-				capabilities = capabilities
+				capabilities = capabilities,
 			})
 			lspconfig.tailwindcss.setup({
-				capabilities = capabilities
+				capabilities = capabilities,
 			})
 			lspconfig.cssls.setup({
-				capabilities = capabilities
+				capabilities = capabilities,
 			})
 			lspconfig.rust_analyzer.setup({
-				capabilities = capabilities
+				capabilities = capabilities,
+			})
+			lspconfig.sourcekit.setup({
+				capabilities = {
+					workspace = {
+						didChangeWatchedFiles = {
+							dynamicRegistration = true,
+						},
+					},
+				},
 			})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
