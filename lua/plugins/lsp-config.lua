3262b28e57bce9e978b519043e73f08fdf0ba3dc
Jannik Donker <jado96@web.de>
2024-11-02 08:32:54 +0100

    removed sourcekit from mason ensure_installed

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "ts_ls", "svelte", "tailwindcss", "cssls", "rust_analyzer", "jedi_language_server", "sourcekit" },
+				ensure_installed = { "lua_ls", "ts_ls", "svelte", "tailwindcss", "cssls", "rust_analyzer", "jedi_language_server" },
 			})
 		end,
 	},
